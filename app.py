43a10bc86c932a026a9d7374a06469755e807912
Nayif05 <161000003+Nayif05@users.noreply.github.com>
2025-03-30 10:49:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -381,4 +381,5 @@ def not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=True)
+
